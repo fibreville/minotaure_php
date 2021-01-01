8d1e2b54c04cbba9c6094aa01a623c8671b14a8d
Cell <>
2021-01-01 15:04:02 +0100

    REMOVE carriage return character

--- src/connexion.php
+++ src/connexion.php
@@ -1,1 +1,10 @@
-<?php  $db = new PDO('mysql:host=mysql;dbname=atrpg;','atrpg','mot-de-passe'); ?>
+<?php
+
+try {
+    $db = new PDO('mysql:host=localhost;dbname=atrpg;', 'root', 'root');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die('Erreur sql');
+}
+?>
