1ba6db1ee36c929ce93a5fb5d3169f9639645454
frgri <francois.grimm@yogarik.com>
2021-01-12 05:43:25 +0100

    update connexion example

--- src/connexion_example.php
+++ src/connexion_example.php
@@ -1,10 +1,22 @@
 <?php
 try {
-    $db = new PDO('mysql:host=serveur-mysql;dbname=nombase;', 'login', 'mdp');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  // Changez les valeurs dans la ligne ci-dessous.
+  $db = new PDO('mysql:host=serveur-mysql;dbname=nombase;', 'login', 'mdp');
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     die('Erreur sql : ' . $e->getMessage());
 }
+// Pour la majorité des hébergements, laissez "/tmp" .
+// Pour 000webhost, mettre "./tmp" .
 $tmp_path = '/tmp';
+
+if (file_exists($tmp_path)) {
+  if (!is_writable($tmp_path)) {
+    die("Dossier des fichiers temporaires non-accessible en écriture.");
+  }
+}
+else {
+  die("Dossier des fichiers temporaires introuvable.");
+}
 
